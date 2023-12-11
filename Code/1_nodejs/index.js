00e80590bad4516453f29ecde01d7ea10d0f77a4
PraveenRaamC <ckpraveenraam@gmail.com>
2023-12-11 19:17:00 +0530

    Day 2 Works are over

--- Code/1_nodejs/index.js
+++ Code/1_nodejs/index.js
@@ -1,9 +1,9 @@
 var http = require("http");
 
-http.createServer(function(requst,response){
-    response.writeHead(200,{'content-Type':'text/plain'});
+http.createServer(function (requst, response) {
+    response.writeHead(200, { 'content-Type': 'text/plain' });
 
-    response.end("Hello World - Praveen Raam C");
+    response.end("Hello World - Praveen Raam C - 7376222IT226");
 }).listen(3000);
 
 console.log("Server runnning in 3000");
